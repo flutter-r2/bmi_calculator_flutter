c758983af36a2a40685243e81def06380e124657
Artur <arturbr@gmail.com>
2022-04-05 22:34:49 -0300

    Creates a card widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,8 +12,54 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: const [
+                Expanded(
+                  child: BMICard(Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: BMICard(Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+          const Expanded(
+            child: BMICard(Color(0xFF1D1E33)),
+          ),
+          Expanded(
+            child: Row(
+              children: const [
+                Expanded(
+                  child: BMICard(Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: BMICard(Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BMICard extends StatelessWidget {
+  final Color color;
+
+  // ignore: use_key_in_widget_constructors
+  const BMICard(this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
